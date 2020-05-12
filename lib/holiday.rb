fbdcca0df0d61e64c9951068b441f9f46f3a98d0
Scott  Keplinger <skeplin17@gmail.com>
2020-05-12 22:54:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_supplies)
 
 def all_holidays_with_bbq(holiday_supplies)
   holiday_names_andbbq = []
-  holiday_names_andbbq = holiday_supplies[:season][:holiday]
+  holiday_names_andbbq = holiday_supplies.keys
   # holiday_names_andbbq << holiday_hash[:season][:holiday][2]
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
