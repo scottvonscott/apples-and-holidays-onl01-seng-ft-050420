4477a55a429361c985dfd9fb4201e0e3093de429
Scott  Keplinger <skeplin17@gmail.com>
2020-05-12 21:14:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,6 +54,7 @@ def all_supplies_in_holidays(holiday_supplies)
            line_b.collect do |words|
             line_d = []
              line_d << words.capitalize
+             binding.pry
        puts "#{line_a}:"
        puts "  #{line_d} #{line_c}"
        end
