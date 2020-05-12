af1bb1dcacf677f5b9c7ff70d764a89050b3d18a
Scott  Keplinger <skeplin17@gmail.com>
2020-05-12 23:48:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_supplies)
 
 def all_holidays_with_bbq(holiday_supplies)
   holiday_supplies.select do |seasons, holidays, supplies|
-    season[:holiday][:supplies] == 'BBQ'
+    seasons[:holiday][:supplies] == 'BBQ'
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 end
